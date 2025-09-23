f6b5307be06b7f70e2689ee0af1b466b636ac80e
Divya-Rasam <divyaarunrasam@gmail.com>
2025-09-23 21:55:32 +0530

    update

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,46 +1,12 @@
-// const express = require('express')                        not used now, was used before
+// const express = require('express')
+
 import express from 'express'
-import path, {dirname} from 'path'
-import { fileURLToPath } from 'url'
-
-
 
 const app = express()
-const PORT = process.env.PORT || 5003
+const PORT = process.env.PORT || 5003;
 
-
-
-
-
-// Get the file path from the URL of the current module
-const __filename = fileURLToPath(import.meta.url)
-// Get the directory name from the file path
-const __dirname = (__filename) 
-
-
-
-
-
-
-// Serves the HTML file from the /public directory
-// Tells express to serve all files from the public folder as static assets / file
-// Any requests for the css files will be resolved   to the public directory
-app.use(express.static(path.join(__dirname, '../public')))
-
-
-
-
-// console.log('hello world!')
-
-
-
-
-// Serving up the HTML file from the /public directory
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-} )
+console.log("Hello World")
 
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`)
 })
-
